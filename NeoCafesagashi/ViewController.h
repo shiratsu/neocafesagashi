2d3b4d6b90b9ab51e91938b414ecfc622b5bc4e7
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2014-04-16 07:14:54 +0900

    Initial Commit

---
+++ NeoCafesagashi/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NeoCafesagashi
+//
+//  Created by HIRATSUKA SHUNSUKE on 2014/04/16.
+//  Copyright (c) 2014年 HIRATSUKA SHUNSUKE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ NeoCafesagashi/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NeoCafesagashi
+//
+//  Created by HIRATSUKA SHUNSUKE on 2014/04/16.
+//  Copyright (c) 2014年 HIRATSUKA SHUNSUKE. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
