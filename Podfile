e1ab19f1d9862bc978a6b747ee9c49c53cd90c6d
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2014-05-17 18:33:41 +0900

    initial commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '7.0'
+
+pod 'SMCalloutView', '~> 2.0'
+# Add Kiwi as an exclusive dependency for the AmazingAppTests target
+target :NeoCafesagashiTests, :exclusive => true do
+   pod 'OCMock'
+   pod 'Kiwi/XCTest'	
+end
+
+
+
---
+++ Pods/Pods-NeoCafesagashiTests-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Kiwi/ARC
+#define COCOAPODS_POD_AVAILABLE_Kiwi_ARC
+#define COCOAPODS_VERSION_MAJOR_Kiwi_ARC 2
+#define COCOAPODS_VERSION_MINOR_Kiwi_ARC 2
+#define COCOAPODS_VERSION_PATCH_Kiwi_ARC 4
+
+// Kiwi/NonARC
+#define COCOAPODS_POD_AVAILABLE_Kiwi_NonARC
+#define COCOAPODS_VERSION_MAJOR_Kiwi_NonARC 2
+#define COCOAPODS_VERSION_MINOR_Kiwi_NonARC 2
+#define COCOAPODS_VERSION_PATCH_Kiwi_NonARC 4
+
+// Kiwi/XCTest
+#define COCOAPODS_POD_AVAILABLE_Kiwi_XCTest
+#define COCOAPODS_VERSION_MAJOR_Kiwi_XCTest 2
+#define COCOAPODS_VERSION_MINOR_Kiwi_XCTest 2
+#define COCOAPODS_VERSION_PATCH_Kiwi_XCTest 4
+
+// OCMock
+#define COCOAPODS_POD_AVAILABLE_OCMock
+#define COCOAPODS_VERSION_MAJOR_OCMock 2
+#define COCOAPODS_VERSION_MINOR_OCMock 2
+#define COCOAPODS_VERSION_PATCH_OCMock 4
+
